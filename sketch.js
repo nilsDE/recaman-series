487449ca0da13a8cedf26be7da1aefcf2fb648da
Nils <39887052+nilsDE@users.noreply.github.com>
2018-07-10 20:57:54 +0200

    Maybe yes but for loop seems to run forever?

--- sketch.js
+++ sketch.js
@@ -22,9 +22,24 @@ function draw() {
 	strokeWeight(3);
 	line(0, height/2, width, height/2);
 
-	strokeWeight(1);
-	stroke(255,0,0);
-	line(racamanSeries[racamanSeries.length-1],
-		height/2-20, racamanSeries[racamanSeries.length-1],
-		height/2);
+  angleMode(DEGREES);
+  let angle = 0;
+  let angleMovement = 0;
+  let circleStartPoint = racamanSeries[racamanSeries.length - 2];
+  let diameter = 0;
+  if (racamanSeries[racamanSeries.length - 1] > racamanSeries[racamanSeries.length - 2]) {
+    diameter = racamanSeries[racamanSeries.length - 1] - racamanSeries[racamanSeries.length - 2];
+    angle = 180;
+    angleMovement = 1;
+  } else {
+    diameter = racamanSeries[racamanSeries.length - 2] - racamanSeries[racamanSeries.length - 1];
+    angle = -180;
+    angleMovement = -1;
+  }
+/*
+  for (i = 0; i = angle; i += angleMovement) {
+    let x = circleStartPoint;
+    let y = height/2;
+  }
+  */
 }
